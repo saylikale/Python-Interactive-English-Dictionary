adc721afdc908cba9b90c3aeff1e54d659d535cc
saylikale <saylikale40@gmail.com>
2020-06-05 14:07:19 +0530

    Handling incorrect/bad words
    if the word doesn't exist in data.json, this code will display the message that the word doesn't exist

--- index.py
+++ index.py
@@ -2,7 +2,10 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-  return data[w]
+  if w in data:
+    return data[w]
+  else:
+    return "The word doesn't exist.Please cross-check the spelling."
 
 word = input("Enter the word: ")
 print(translate(word))
